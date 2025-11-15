5cde8ff25af39ebe485400857800a02571715ed3
Drackin <sonaycannet10@gmail.com>
2025-11-15 16:33:28 +0300

    refactor: Connection check changed to gstatic

--- src-tauri/src/helpers/net_helper.rs
+++ src-tauri/src/helpers/net_helper.rs
@@ -1,23 +1,8 @@
-use std::process::{Command, Stdio};
-
 #[tauri::command]
-pub fn check_connection() -> bool {
-    let mut command = Command::new("ping");
+pub async fn check_connection() -> bool {
+    let response = reqwest::get("https://www.gstatic.com/generate_204");
 
-    #[cfg(target_os = "windows")]
-    #[allow(unused_imports)]
-    {
-        use std::os::windows::process::CommandExt;
-        command.creation_flags(0x08000000);
-    }
-
-    command.arg("8.8.8.8").stdout(Stdio::null()).stderr(Stdio::null());
-
-    let child = command.spawn().expect("Failed to spawn ping");
-
-    let output = child.wait_with_output().expect("ping");
-
-    if output.status.success() {
+    if response.await.is_ok() {
         return true
     }
 
